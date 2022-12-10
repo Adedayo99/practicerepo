fb6ea06d7c279c77e323065bed11e9099e06b47c
Adedayo99 <ajibadeadedayo99@gmail.com>
2022-12-10 09:30:05 -0800

    upgrade

--- shell.c
+++ shell.c
@@ -1,77 +1,82 @@
-/**
-* sample shell program
-*/
+
+
 #include <unistd.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
 
+int command_check(char *token);
+int twostrcmp(char a[],char b[]);
+
 int main(void)
 {
-	int fork_val;
-	char input[200];
+    //int fork_val;
+
 	char *delim = " ";
 	char *token;
-	char *ptr[];
 	char *buf = NULL;
-	size_t n = 0;
+	int flag = 0;
+	size_t val;
+	int exit_val = 1;
+
+    size_t n = 0;
 
 	while (1)
 	{
 		write(STDOUT_FILENO, "($) ", 4);
-		getline(&buf, &n, stdin);
+		val = getline(&buf, &n, stdin);
 
 		token = strtok(buf, delim);
-		
 
-		if (*buf != 'q')
-		printf("input is: %s", buf);
+		exit_val = twostrcmp(token, "exit");
+	        if (exit_val == 0)
+        	return (0);
 
-		else
-		break;
+	        flag = command_check(token);
+        	if (flag == 0)
+            	printf("command present\n");
+
+		if (flag == 1)
+		printf("command absent\n");
+
+
 	}
-/*
 
-	fork_val = fork();
-*/
-	return (0);	
+	return (0);
 }
 
 
-  
-int twoStrcmp(char a[],char b[])  
-{  
-	int flag = 0;
-	i = 0;  
 
-	while (a[i]!='\0' &&b[i]!='\0')  
-	{  
-		if (a[i] != b[i])  
-		{  
-			flag = 1;  
-			break;  
-		}  
-		i++;  
-	}
- 
-	if (a[i]!='\0'||b[i]!='\0')
-	return (1);
-   
-	if (flag == 0)  
-	return (0);  
-    
-	else  
-	return (1);  
-}
-
-int command_check(char *ptr, char *delim)
+int twostrcmp(char a[],char b[])
 {
-	char *list[3] = {"cd", "pwd", "ls", NULL};
+	int flag = 0;
+	int i = 0;
 
-
-	for (i = 0; list[i] != '\0'; i++)
+	while (a[i] != '\0' && b[i] != '\0')
 	{
-		flag = twostrcmp(ptr, list[i];
+		if (a[i] != b[i])
+		{
+			flag = 1;
+			return (flag);
+		}
+		i++;
+	}
+
+	return (flag);
+}
+
+int command_check(char *token)
+{
+	int flag = 0;
+	int i;
+	char *list[4] = {"cd", "pwd", "ls", NULL};
+
+
+	for (i = 0; list[i] != NULL; i++)
+	{
+		flag = twostrcmp(token,list[i]);
+		if (flag == 0)
+		return (flag);
 	}
 
 	return (flag);
--- shell_save.c
+++ shell_save.c
@@ -1,30 +1,83 @@
-/**
-* sample shell program
-*/
+
+
 #include <unistd.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
+
+int command_check(char *token);
+int twostrcmp(char a[],char b[]);
 
 int main(void)
 {
-	int fork_val;
+    //int fork_val;
+
+	char *delim = " ";
+	char *token;
 	char *buf = NULL;
+	int flag = 0;
+	size_t val;
+	int exit_val = 1;
+
 	size_t n = 0;
 
 	while (1)
 	{
 		write(STDOUT_FILENO, "($) ", 4);
-		getline(&buf, &n, stdin);
-		
-		if (*buf != 'q')
-		printf("input is: %s", buf);
+		val = getline(&buf, &n, stdin);
 
-		else
-		break;
+		token = strtok(buf, delim);
+
+		exit_val = twostrcmp(token, "exit");
+	        if (exit_val == 0)
+        	return (0);
+
+	        flag = command_check(token);
+        	if (flag == 0)
+            	printf("command present\n");
+
+		if (flag == 1)
+		printf("command absent\n");
+
+
 	}
-/*
 
-	fork_val = fork();
-*/
-	return (0);	
+	return (0);
+}
+
+
+
+int twostrcmp(char a[],char b[])
+{
+	int flag = 0;
+	int i = 0;
+
+	while (a[i] != '\0' && b[i] != '\0')
+	{
+		if (a[i] != b[i])
+		{
+			flag = 1;
+			return (flag);
+		}
+		i++;
+	}
+
+	return (flag);
+}
+
+int command_check(char *token)
+{
+	int flag = 0;
+	int i;
+	char *list[4] = {"cd", "pwd", "ls", NULL};
+
+
+	for (i = 0; list[i] != NULL; i++)
+	{
+		flag = twostrcmp(token,list[i]);
+		if (flag == 0)
+		return (flag);
+	}
+
+	return (flag);
 }
