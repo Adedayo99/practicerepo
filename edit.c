944cb2d8a06ca72e9c5337c47dbbb1616d7c11bd
Adedayo99 <111497924+Adedayo99@users.noreply.github.com>
2022-12-13 22:14:06 +0100

    Update edit.c

--- edit.c
+++ edit.c
@@ -1,1 +1,21 @@
+#include "shell.h"
 
+/**
+* main - mother function that runs the whole process
+* argc: argument count
+*@argv: argument vector
+*@envp: environment vector
+*
+* Return: int value to indicate success or failure
+*/
+
+int main(void)
+{
+        int fork_val;
+	int i;
+	char *buf = NULL;
+	int flag = 0;
+	size_t val;
+	char **arg = NULL;
+	char **arggs = NULL;
+        size_t n = 0;
